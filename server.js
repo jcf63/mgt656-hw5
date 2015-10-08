bef3df377c4d62094b4e6662209148c91399ce58
jcf63 <jeremy.foo@yale.edu>
2015-10-08 04:21:33 +0000

    Add nickname route

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(request, response){
-    response.send('Hello MGT-656!');
+    response.send('Hello goodbye!');
+});
+
+app.get('/perfect-octopus', function(request, response){
+    response.send('Apparently I am a perfect octopus even though I have 7 tentacles');
 });
 
 app.listen(process.env.PORT || 4000);
